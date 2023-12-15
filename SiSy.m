77414a13c4c64fb2edd456585c8e242c23cc82aa
Sebastian Grünwald <sebiscodes@gmail.com>
2023-12-15 09:16:58 +0100

    Bugfixes and time coefficients have been added
    Attention: TimePerPeriod has been changed to SamplesPerPeriod!
    Offset errors are now fixed.

--- SiSy.m
+++ SiSy.m
@@ -16,6 +16,7 @@ classdef SiSy
         o_t (1,:) double % The time calculated by the x array and Ts
         o_N uint64 % The amount of values in the measurment
         o_Np uint64 % The amount of values in a period
+        o_offset uint64 % The offset of the period part
 
         % Characteristics
         o_maxS double % Maximal value of the signal
@@ -38,6 +39,17 @@ classdef SiSy
         % Convolution
         conv_t (1,:) double % The time vector of the convolution
         conv_s (1,:) double % The signal vector of the convolution
+
+        % Fourier-Coefficients
+        coeff_s (1,:) double % The part of the function which we get the fourier-coefficients from
+        coeff_t (1,:) double % The part of the time value which we get the fourier-coefficients from
+        coeff_Ak (1,:) double % Ak coefficients
+        coeff_Bk (1,:) double % Bk coefficients
+        coeff_Mk (1,:) double % Mk coefficients
+        coeff_N uint64 % Amount of coefficients
+        coeff_y (1,:) double % Approximated function
+        coeff_yA (1,:) double % Approximated function using only A-coefficients
+        coeff_yB (1,:) double % Approximated function using only B-coefficients
     end
     
     methods
@@ -45,46 +57,46 @@ classdef SiSy
             %SISY Construct an instance of this class
         end
         
-        function obj = addWav(obj, path, time_of_one_period, offset)
+        function obj = addWav(obj, path, samplesPerPeriod, offset)
             arguments
                 obj
                 path string
-                time_of_one_period double = 0
+                samplesPerPeriod uint64 = 0
                 offset uint64 = 0
             end
             [obj.o_s,obj.o_fs] = audioread(path);
-            obj = obj.setSignal(obj.o_s, obj.o_fs,time_of_one_period, offset);
+            obj = obj.setSignal(obj.o_s, obj.o_fs,samplesPerPeriod, offset);
         end
 
         %% Store a signal to this class
-        function obj = setSignal(obj, signal_values,frequency, time_of_one_period, offset)
+        function obj = setSignal(obj, signal_values,sampleFrequency, samplesPerPeriod, offset)
             arguments
                 obj
                 signal_values (1,:) string
-                frequency double
-                time_of_one_period double = 0
+                sampleFrequency double
+                samplesPerPeriod uint64 = length(signal_values)
                 offset uint64 = 0
             end
             % Signal calculations
+            obj.o_offset = offset;
             obj.o_s = signal_values;
             obj.o_N = length(obj.o_s);
             obj.o_x = (0:(obj.o_N-1));
-            obj.o_fs = frequency;
+            obj.o_fs = sampleFrequency;
             obj.o_Ts = 1/obj.o_fs;
-            obj.o_Tp = time_of_one_period;
+            obj.o_Np = samplesPerPeriod;
+            obj.o_Tp = samplesPerPeriod*obj.o_Ts;
             obj.o_t = double(obj.o_x)*obj.o_Ts;
             if obj.o_Tp == 0
                 obj.o_Tp = obj.o_t(1,end);
             end 
 
-            obj.o_Np = obj.o_Tp/obj.o_Ts;
-
             % Get the characteristics of the function
             obj.o_minS = min(obj.o_s);
             obj.o_maxS = max(obj.o_s);
-            [obj, obj.o_rms, obj.o_P] = obj.getRMSandP(offset);
-            [obj, obj.o_integral_t, obj.o_integral_s, obj.o_area, obj.o_absarea] = obj.getIntegral(offset);
-            [obj, obj.fft_t, obj.fft_s, obj.fft_f, obj.fft_y] = obj.getFFT(offset);
+            [obj, obj.o_rms, obj.o_P] = obj.getRMSandP();
+            [obj, obj.o_integral_t, obj.o_integral_s, obj.o_area, obj.o_absarea] = obj.getIntegral();
+            [obj, obj.fft_t, obj.fft_s, obj.fft_f, obj.fft_y] = obj.getFFT();
         end
 
         %% Get rms and power of the signal by index offset
@@ -92,12 +104,12 @@ classdef SiSy
         function [obj, rms, P] = getRMSandP(obj, offset, N)
             arguments
                 obj
-                offset uint64 % The offset to your signal
+                offset uint64 = obj.o_offset % The offset to your signal
                 N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
             end
             offset = offset + 1;
             obj.o_P = 0;
-            for n = offset+1:offset+N
+            for n = offset:offset+N-1
                 obj.o_P = obj.o_P + obj.o_s(n)*obj.o_s(n);
             end
             obj.o_P = obj.o_P / obj.o_Tp;
@@ -107,10 +119,10 @@ classdef SiSy
         end
 
         %% Get the x and y of the signal
-        function [x,y, f, N] = getSignal(obj)
+        function [x,y, fs, N] = getSignal(obj)
             x = obj.o_t;
             y = obj.o_s;
-            f = obj.o_fs;
+            fs = obj.o_fs;
             N = obj.o_N;
         end
 
@@ -118,19 +130,22 @@ classdef SiSy
         function [obj, t_integral, s_integral, area, absarea] = getIntegral(obj, offset, N)
             arguments
                 obj
-                offset uint64 = 0 % The offset to your signal
+                offset uint64 = obj.o_offset % The offset to your signal
                 N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
             end
             obj.o_area = 0;
             obj.o_absarea = 0;
-            obj.o_integral_s = obj.o_s(1:N)*0;
-            obj.o_integral_t = obj.o_s(1:N)*0;
+            obj.o_integral_s = zeros(1,N);
+            obj.o_integral_t = zeros(1,N);
+            obj.o_integral_t(1) = obj.o_t(1);
+            obj.o_integral_s(1) = 0;
             %obj.o_integral_t(1) = obj.o_t(1+offset);
             %obj.o_integral_s(1) = obj.o_s(1+offset);
-            for n = 2+offset:obj.o_N+offset
-                obj.o_integral_t(n-offset) = obj.o_t(n);
-                addVal =  obj.o_s(n-1)*(obj.o_t(n)-obj.o_t(n-1))+(obj.o_s(n)-obj.o_s(n-1))*(obj.o_t(n)-obj.o_t(n-1))/2;
-                obj.o_integral_s(n-offset) = obj.o_integral_s(n-1) + addVal;
+            for n = 2:N
+                obj.o_integral_t(n) = obj.o_t(n);
+                n_offset = n+offset;
+                addVal =  obj.o_s(n_offset-1)*(obj.o_t(n_offset)-obj.o_t(n_offset-1))+(obj.o_s(n_offset)-obj.o_s(n_offset-1))*(obj.o_t(n_offset)-obj.o_t(n_offset-1))/2;
+                obj.o_integral_s(n) = obj.o_integral_s(n-1) + addVal;
                 obj.o_area = obj.o_area + addVal;
                 obj.o_absarea = obj.o_absarea + abs(addVal);
             end
@@ -145,7 +160,7 @@ classdef SiSy
         function [xp,yp] = getPSignal(obj, offset, N)
             arguments
                 obj
-                offset uint64 = 0 % The offset to your signal
+                offset uint64 = obj.o_offset % The offset to your signal
                 N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
             end
             xp = obj.o_t(1+offset:1+offset+N);
@@ -165,15 +180,15 @@ classdef SiSy
         function [obj, fft_t, fft_s, fft_f, fft_y] = getFFT(obj, offset, N)
             arguments
                 obj
-                offset uint64 = 0 % The offset to your signal x(offset:offset+N-1)
+                offset uint64 = obj.o_offset % The offset to your signal x(offset:offset+N-1)
                 N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
             end
             offset = offset + 1;
             obj.fft_start = offset;
             obj.fft_N = N;
-            obj.fft_t = obj.o_t(obj.fft_start:obj.fft_start+obj.fft_N);
-            obj.fft_f = [0:obj.fft_N]*obj.o_fs/obj.fft_N;
-            obj.fft_s = obj.o_s(obj.fft_start:obj.fft_start+obj.fft_N);
+            obj.fft_t = obj.o_t(obj.fft_start:obj.fft_start+obj.fft_N-1);
+            obj.fft_f = [0:obj.fft_N-1]*obj.o_fs/obj.fft_N;
+            obj.fft_s = obj.o_s(obj.fft_start:obj.fft_start+obj.fft_N-1);
             obj.fft_y = abs(fft(obj.fft_s));
             fft_t = obj.fft_t;
             fft_s = obj.fft_s;
@@ -185,7 +200,7 @@ classdef SiSy
             arguments
                 obj
                 h (1,:) double % The impulse response
-                offset uint64 = 0 % The offset to your signal x(offset:offset+N-1)
+                offset uint64 = obj.o_offset % The offset to your signal x(offset:offset+N-1)
                 N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
             end
             offset = offset + 1;
@@ -194,6 +209,58 @@ classdef SiSy
             obj.conv_s = obj.conv_s(offset:offset+N);
             conv_t = obj.conv_t;
             conv_s = obj.conv_s;
+        end
+
+        function [obj, coeff_t, coeff_s, approx_s, approx_sA, approx_sB, Ak, Bk, Mk] = getFourierCoefficients(obj, N_coefficients, offset, N)
+            arguments
+                obj
+                N_coefficients uint64 = 11 % Amount of fourier-coefficients to calculate
+                offset uint64 = obj.o_offset % The offset to your signal x(offset:offset+N-1)
+                N uint64 = obj.o_Np % The amount of samples from the offset x(offset:offset+N-1)
+            end
+            offset = offset + 1;
+            obj.coeff_N = N_coefficients;
+            obj.coeff_t = obj.o_t(offset:offset+N-1);
+            obj.coeff_s = obj.o_s(offset:offset+N-1);
+
+            obj.coeff_Ak = zeros(1,obj.coeff_N);
+            obj.coeff_Bk = zeros(1,obj.coeff_N);
+            obj.coeff_Mk = zeros(1,obj.coeff_N);
+            %obj.coeff_Ak(1) = mean(obj.coeff_s)*2;
+            for k = 1:obj.coeff_N
+                for n = 1:N
+                    obj.coeff_Ak(k) = obj.coeff_Ak(k) + obj.coeff_s(n)*cos(2.0*pi*double((k-1)*n)/double(N));
+                    obj.coeff_Bk(k) = obj.coeff_Bk(k) + obj.coeff_s(n)*sin(2.0*pi*double((k-1)*n)/double(N));
+                end
+                obj.coeff_Ak(k)=(2.0/double(N))*obj.coeff_Ak(k);
+                obj.coeff_Bk(k)=(2.0/double(N))*obj.coeff_Bk(k);
+                obj.coeff_Mk(k)=sqrt(obj.coeff_Ak(k)^2+obj.coeff_Bk(k)^2);
+            end
+            obj.coeff_y = zeros(1,N);
+            obj.coeff_yA = zeros(1,N);
+            obj.coeff_yB = zeros(1,N);
+
+            obj.coeff_y = obj.coeff_Ak(1)*(1.0/2.0);
+            obj.coeff_yA = obj.coeff_Ak(1)*(1.0/2.0);
+            for n = 1:obj.coeff_N-1
+                %obj.coeff_y = obj.coeff_y - obj.coeff_yA - obj.coeff_yB;
+                %obj.coeff_yA = obj.coeff_yA+obj.coeff_Ak(n+1)*cos(2.0*pi*double(n)*obj.o_fs*obj.coeff_t);
+                %obj.coeff_yB = obj.coeff_yB+obj.coeff_Bk(n+1)*sin(2.0*pi*double(n)*obj.o_fs*obj.coeff_t);
+                Ak_temp = obj.coeff_Ak(n+1)*cos(pi/2.0*(1.0/obj.o_fs)*double(n)*(double(N))*obj.coeff_t);
+                Bk_temp = obj.coeff_Bk(n+1)*sin(pi/2.0*(1.0/obj.o_fs)*double(n)*(double(N))*obj.coeff_t);
+                obj.coeff_yA = obj.coeff_yA+Ak_temp;
+                obj.coeff_yB = obj.coeff_yB+Bk_temp;
+                obj.coeff_y = obj.coeff_y + Ak_temp+Bk_temp;
+            end
+            
+            coeff_t = obj.coeff_t;
+            coeff_s = obj.coeff_s;
+            Ak = obj.coeff_Ak;
+            Bk = obj.coeff_Bk;
+            Mk = obj.coeff_Mk;
+            approx_s = obj.coeff_y;
+            approx_sA = obj.coeff_yA;
+            approx_sB = obj.coeff_yB;
         end
     end
 end
--- SiSy_Examples.m
+++ SiSy_Examples.m
@@ -12,8 +12,8 @@ sisyObj = sisyObj.addWav("dtmf_signal.wav", 1/20); % Second param is the period 
 
 disp(sisyObj); % Show values of the sisy object
 
-subplot(1,1,1), plot(t,s); % Plot signal
-grid; xlabel('t / s'); ylabel('x(t)');
+subplot(1,1,1), plot(t,s);grid; % Plot signal
+grid;
 
 
 
@@ -30,8 +30,8 @@ sisyObj = sisyObj.setSignal(s, fs); % Second param is the period length in secon
 
 disp(sisyObj); % Show values of the sisy object
 
-subplot(1,1,1), plot(t2,s2); % Plot signal
-grid; xlabel('t / s'); ylabel('x(t)');
+subplot(1,1,1), plot(t2,s2);grid; % Plot signal
+grid;
 
 
 
@@ -45,11 +45,11 @@ sisyObj = sisyObj.addWav("dtmf_signal.wav", 1/20); % Second param is the period 
 
 disp(sisyObj); % Show values of the sisy object
 
-subplot(3,1,1), plot(t,s); % Plot signal
-subplot(3,1,2), plot(t_p,s_p); % Plot part signal
-subplot(3,1,3), plot(fft_f,fft_s); % Plot fft signal
+subplot(3,1,1), plot(t,s);grid; % Plot signal
+subplot(3,1,2), plot(t_p,s_p);grid; % Plot part signal
+subplot(3,1,3), plot(fft_f,fft_s);grid; % Plot fft signal
 
-grid; xlabel('t / s'); ylabel('x(t)');
+grid;
 
 
 
@@ -61,15 +61,16 @@ s = sin(2*pi*t); % Sin signal over t
 fs = 100; % Frequency: 100 samples per second
 
 sisyObj = SiSy; % Init SiSy Object
-sisyObj = sisyObj.setSignal(s, fs); % Second param is the period length in seconds
+sisyObj = sisyObj.setSignal(s, fs, 50, 25); % Second param is the period length in seconds
+%sisyObj = sisyObj.setSignal(s, fs); % Integrate the whole function
 [t2,s2,f,N] = sisyObj.getSignal(); % Get the signal and its frequency and sammple amount
 [sisyObj, i_t, i_s, area, absarea] = sisyObj.getIntegral();
 
 disp(sisyObj); % Show values of the sisy object
 
-subplot(2,1,1), plot(t2,s2); % Plot signal
-subplot(2,1,2), plot(i_t,i_s); % Plot integral
-grid; xlabel('t / s'); ylabel('x(t)');
+subplot(2,1,1), plot(t2,s2);grid; % Plot signal
+subplot(2,1,2), plot(i_t,i_s);grid; % Plot integral
+grid;
 
 
 
@@ -77,7 +78,7 @@ grid; xlabel('t / s'); ylabel('x(t)');
 clear; close all; clc;
 
 [x,fs] = audioread('aufgabe_4.wav');
-x = x'; % Signal x ist ein Zeilenvektor
+x = x'; % Column to row
 
 sisyObj = SiSy; % Init SiSy Object
 sisyObj = sisyObj.addWav("aufgabe_4.wav"); % Second param is the period length in seconds
@@ -91,6 +92,47 @@ h = (sisyObj.o_Ts/tau) * exp(-t/tau);% Impulse response
 
 disp(sisyObj); % Show values of the sisy object
 
-subplot(2,1,1), plot(t,s,conv_t, conv_s); % Plot signal and convolution
+subplot(2,1,1), plot(t,s,conv_t, conv_s);grid; % Plot signal and convolution
 
-grid; xlabel('t / s'); ylabel('x(t)');
+
+
+
+%% Get Ak & Bk coefficients
+clear; close all; clc;
+
+% Parameter
+f0 = 1;     % Frequency
+N = 2000;   % Amount of samples
+fs = 1000;  % Sampling Frequency
+
+t = [0:N-1]*(2/2000);   % Time 
+
+s=sign(sin(2*pi*f0*(t-0.4)))+sin(t); %Rechtecksignal
+
+N_coeff = 32; % Amount of Ak, Bk and Mk coefficients
+
+sisyObj = SiSy; % Init SiSy Object
+sisyObj = sisyObj.setSignal(s, fs); % Second param is the period length in seconds
+%sisyObj = sisyObj.setSignal(s, fs, 1000, 500); % ATTENTION! MIGHT BE BUGGY! Second param is the period length in seconds
+[t2,s2,fs,N] = sisyObj.getSignal(); % Get the signal and its frequency and sammple amount
+
+[sisyObj, ct, cs, aS, aSA, aSB, Ak, Bk, Mk] = sisyObj.getFourierCoefficients(N_coeff);
+
+disp(sisyObj); % Show values of the sisy object
+
+subplot(4,2,1), plot(t2,s2); % Plot signal
+grid; xlabel('t / s'); ylabel('s(t)');
+subplot(4,2,2), plot(ct,cs); % Plot signal
+grid; xlabel('t / s'); ylabel('part of s(t)');
+subplot(4,2,3), plot(ct,aS); % Plot approximated signal
+grid; xlabel('t / s'); ylabel('Approximated part of s(t)');
+subplot(4,2,5), plot(ct,aSA); % Plot A-part signal
+grid; xlabel('t / s'); ylabel('A-part');
+subplot(4,2,7), plot(ct,aSB); % Plot B-part signal
+grid; xlabel('t / s'); ylabel('B-part');
+subplot(4,2,4), stem([1:sisyObj.coeff_N],Ak); hold on; % Plot B-part signal
+grid; xlabel('t / s'); ylabel('Ak');
+subplot(4,2,6), stem([1:sisyObj.coeff_N],Bk); % Plot B-part signal
+grid; xlabel('t / s'); ylabel('Bk');
+subplot(4,2,8), stem([1:sisyObj.coeff_N],Mk); % Plot B-part signal
+grid; xlabel('t / s'); ylabel('Mk');
